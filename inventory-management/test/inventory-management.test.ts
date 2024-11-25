2edf57de48cc0328e9c13a4041823db017d3ce72
JulianDGP <juliandgp@hotmail.com>
2024-11-25 17:24:19 -0500

    Inicializacion y estructuracion del proyecto

---
+++ inventory-management/test/inventory-management.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as InventoryManagement from '../lib/inventory-management-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/inventory-management-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new InventoryManagement.InventoryManagementStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
